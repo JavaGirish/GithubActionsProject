bb3701ed0e655c9bdb7856c41d16c3ed9777f83c
Girish Nair <gnair@csod.com>
2022-07-07 21:41:26 +0530

    initial commit

---
+++ src/test/java/com/tests/AmazonTest.java
@@ -0,0 +1,55 @@
+package com.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AmazonTest {
+
+    private static WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofMinutes(1));
+        driver.get("https://www.amazon.in/");
+
+    }
+
+    @Test(priority = 1)
+    public void verifyTitleIsAsExpected() {
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in");
+
+    }
+
+    @Test(priority = 2)
+    public void verifyLogoIsDisplayed() {
+        Assert.assertTrue(driver.findElement(By.cssSelector("#nav-logo-sprites")).isDisplayed());
+    }
+
+    @Test(priority = 3)
+    public void SearchTest() {
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Apple Macbook Air");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//span[contains(@class,'a-size-medium')]"));
+        elements.stream().filter(e -> e.getText().contains("Macbook Air")).forEach(e -> System.out.println(e.getText()));
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+
+    }
+}
