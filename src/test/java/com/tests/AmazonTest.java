bc37e11d46221b7d80bbaf6e961e64dce41058b7
Girish Nair <nairgirish100@gmail.com>
2022-08-07 17:39:32 +0530

    updated to cross browser tests

--- src/test/java/com/tests/AmazonTest.java
+++ src/test/java/com/tests/AmazonTest.java
@@ -1,29 +1,28 @@
 package com.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.List;
 
 public class AmazonTest {
 
     private WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
+    @Parameters({"browser", "version"})
+    public void setUp(String browserName, String version, Method method) {
         /*
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
@@ -32,13 +31,15 @@ public class AmazonTest {
         options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
         WebDriverManager.chromedriver().setup();
         */
+        String testMethodName = method.getName();
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        capabilities.setVersion("80.0");
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, browserName);
+        capabilities.setCapability(CapabilityType.BROWSER_VERSION, version);
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("name", testMethodName);
         try {
-            driver = new RemoteWebDriver(new URL("http://34.131.172.95:4444/wd/hub"), capabilities);
+            driver = new RemoteWebDriver(new URL("http://34.131.87.254:4444/wd/hub"), capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -59,14 +60,6 @@ public class AmazonTest {
     @Test(priority = 2)
     public void verifyLogoIsDisplayed() {
         Assert.assertTrue(driver.findElement(By.cssSelector("#nav-logo-sprites")).isDisplayed());
-    }
-
-    @Test(priority = 3)
-    public void SearchTest() {
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Apple Macbook Air");
-        driver.findElement(By.id("nav-search-submit-button")).click();
-        List<WebElement> elements = driver.findElements(By.xpath("//span[contains(@class,'a-size-medium')]"));
-        elements.stream().filter(e -> e.getText().contains("Macbook Air")).forEach(e -> System.out.println(e.getText()));
     }
 
 
